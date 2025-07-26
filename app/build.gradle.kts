8044bb7f246e21fe6da18fedf9e444fb16f90026
yjpark85 <150774548+yjpark85@users.noreply.github.com>
2025-07-27 02:35:20 +0900

    build.gradle.kts 업데이트
    fix: 타겟 SDK 버전 조정 및 중복 의존성 정리
    
    - `targetSdk` 버전을 35에서 **34**로 변경하여 특정 라이브러리 호환성 문제를 해결했습니다.
    - `libs.versions.toml` 파일에서 이미 관리되고 있는 **카카오맵 SDK 의존성(`kakao-mapsdk`)**을 `dependencies` 블록에서 제거하여 중복을 없애고 설정을 깔끔하게 정리했습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         applicationId = "com.example.swuniMAP"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.maps)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
